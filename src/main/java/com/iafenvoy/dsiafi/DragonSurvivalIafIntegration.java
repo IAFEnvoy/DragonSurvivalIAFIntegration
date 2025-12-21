dc7351c206d13372fd77c548164d94f03e42fb57
IAFEnvoy <tanksheng545@126.com>
2025-12-21 13:10:51 +0800

    some new things

--- src/main/java/com/iafenvoy/dsiafi/DragonSurvivalIafIntegration.java
+++ src/main/java/com/iafenvoy/dsiafi/DragonSurvivalIafIntegration.java
@@ -1,16 +1,28 @@
 package com.iafenvoy.dsiafi;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.food.Foods;
+import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.registries.RegisterEvent;
 import org.slf4j.Logger;
 
 @Mod(DragonSurvivalIafIntegration.MOD_ID)
+@EventBusSubscriber
 public class DragonSurvivalIafIntegration {
     public static final String MOD_ID = "dragon_survival_iaf_integration";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public DragonSurvivalIafIntegration(IEventBus bus, ModContainer container) {
+    @SubscribeEvent
+    public static void registerItem(RegisterEvent event) {
+        if (event.getRegistryKey() == Registries.ITEM)
+            event.register(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(MOD_ID, "golden_pork"), () -> new Item(new Item.Properties().food(Foods.COOKED_PORKCHOP)));
     }
 }
